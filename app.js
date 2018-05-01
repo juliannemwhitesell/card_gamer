7c08c747bf0cb1a816e961a279a36eae10f3b316
Francis Whitesell <francisgwhitesell@gmail.com>
2018-04-30 21:40:09 -0400

    add mongoose package and a basic post endpoint to save games (currently broken).

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+//Mongo setup code: this can be abstracted away from the main app
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/game", function(err) {
+ 	// If no error, successfully connected
+ 	if (!err){
+  		console.log ("connected to localhost:27017/game");
+	}
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -14,6 +24,32 @@ app.post("/", function (req, res) {
 	let received = req.body;
 	let payload = { "received" : received };
 	res.send(payload);
+});
+
+//Experimental Mongo code for endpoint
+var gameSchema = new mongoose.Schema({
+  gameId: String,
+  playerIds: Array
+});
+
+var Game = mongoose.model("Game", gameSchema);
+
+app.post("/game", (req, res) => {
+	let received = req.body;
+	//TODO: call the validate function
+	//TODO: call the save function
+	var newGame = new Game(received);
+  	newGame.save()
+    	.then(item => {
+    		//find out how to get the item's id and add it to received before sending.
+      		res.send({ "saved" : received });
+    	})
+    	.catch(err => {
+      		res.status(400).send("Unable to save game to database");
+    	});
+    //TODO: does a mongo save give you the id? We need to provide that to the user on the response.
+	//let payload = { "received" : received };
+	//res.send(payload);
 });
  
 app.listen(3000, "0.0.0.0");
