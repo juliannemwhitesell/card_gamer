b2e37f69adf292f5e9642e0f86fa35825ae0727d
Francis Whitesell <francisgwhitesell@gmail.com>
2018-05-06 17:18:13 -0400

    refactored routes from main app to routes directory (this took all the mongoose logic with it, so that's still a todo)

--- app.js
+++ app.js
@@ -4,63 +4,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var ObjectID = require('mongodb').ObjectID;
-var mongoose = require("mongoose");
-
-/* Local imports*/
-var Game = require("./models/game");
-
-//Mongo setup code: this can be abstracted away from the main app
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/game", function(err) {
- 	// If no error, successfully connected
- 	if (!err){
-  		console.log ("connected to localhost:27017/game");
-	}
-});
+var routes = require('./routes/routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(routes);
+
 app.get("/", function (req, res) {
   res.send({"here": "gone"});
 });
-
-app.post("/", function (req, res) {
-	let received = req.body;
-	let payload = { "received" : received };
-	res.send(payload);
-});
-
-app.post("/game", (req, res) => {
-	let received = req.body;
-	//TODO: call the validate function
-	//TODO: call the save function
-	var newGame = new Game(received);
-	newGame._id = ObjectID();
-	console.log(newGame);
-  	newGame.save()
-    	.then(item => {
-      		res.send({ "saved" : newGame });
-    	})
-    	.catch(err => {
-      		res.status(400).send("Unable to save game to database: " + err);
-    	});
-});
-
-app.get("/game/:id", (req, res) => {
-	Game.find({ _id: req.params.id }, function(err, game) {
-		if (err){ 
-			if (null == game){
-				res.status(404).send();
-			} else {
-				res.status(500).send("Could not find user: " + err);
-			}
-		} else {
-			res.send(game);
-		}
-	});
-})
  
 app.listen(3000, "0.0.0.0");
 
